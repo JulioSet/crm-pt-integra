e20430ca3413615bcd64ecb593a50d975b5c8b2c
JulioSet <105445347+JulioSet@users.noreply.github.com>
2025-02-15 01:25:29 +0700

    update usage of save message method and fetch message

--- src/app/api/chat/accept/route.js
+++ src/app/api/chat/accept/route.js
@@ -1,10 +1,13 @@
+import { saveMessageToDatabase } from "@/lib/message";
+
 export async function POST(req) { // It should be POST or GET
   try {
     const data = await req.json(); // Parse the incoming JSON
     console.log("Received data:", data);
 
     // saving message to database
-    saveMessageToDatabase(data);
+    const { messageId, from, text, timestamp } = data;
+    saveMessageToDatabase(messageId, from, text, timestamp, 'client');
 
     return new Response(JSON.stringify({ status: "Message processed successfully" }), {
       status: 200,
@@ -17,14 +20,4 @@ export async function POST(req) { // It should be POST or GET
       headers: { "Content-Type": "application/json" },
     });
   }
-}
-
-async function saveMessageToDatabase(messageData) {
-  const { messageId, from, text, timestamp } = messageData;
-
-  // console.log(`From: ${from}`); # 6287732919
-  // console.log(`Message: ${text}`); # this is message
-  // console.log(`Timestamp: ${new Date(timestamp * 1000)}`); # Wed Nov 20 2024 16:53:39 GMT+0700 (Indochina Time)
-
-
 }
--- src/lib/message.ts
+++ src/lib/message.ts
@@ -1,3 +1,5 @@
+import { prisma } from "./prisma";
+
 export async function sendMessage(to: string, text: string) {
    const response = await fetch('/api/chat/send', {
       method: 'POST',
@@ -8,7 +10,50 @@ export async function sendMessage(to: string, text: string) {
    });
 
    if (!response.ok) {
-      console.error('Failed to send message');
-      return;
+      return false;
    }
+}
+
+export async function saveMessageToDatabase(messageId: string, from: string, text: string, timestamp: string, responder: string) { 
+   // console.log(`From: ${from}`); # 6287732919
+   // console.log(`Message: ${text}`); # this is message
+   // console.log(`Timestamp: ${new Date(timestamp * 1000)}`); # Wed Nov 20 2024 16:53:39 GMT+0700 (Indochina Time)
+   
+   const check_message = await prisma.message_header.findFirst({
+      where: {telepon: from}
+   });
+
+   if (check_message === null) {
+      await prisma.message_header.create({
+         data: {
+            telepon: from,
+            pesan_terbaru: text,
+            waktu_terbaru: timestamp,
+            label: 'new',
+            akses: 'admin'
+         }
+      });
+   }
+
+   await prisma.message_content.create({
+      data: {
+         message_header_telepon: from,
+         pesan: text,
+         waktu: timestamp,
+         responder: responder
+      }
+   })
+}
+
+export async function getConversations() {
+   const response = await fetch('/api/chat/fetch', {
+      method: 'GET',
+   });
+
+   if (!response.ok) {
+      return false;
+   }
+
+   const data = await response.json()
+   return data;
 }
