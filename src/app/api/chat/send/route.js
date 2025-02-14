734072e332c5268d0065dabd61d9487fa9551919
JulioSet <105445347+JulioSet@users.noreply.github.com>
2025-02-15 04:58:50 +0700

    fixing error and add save database feature

--- src/app/api/chat/send/route.js
+++ src/app/api/chat/send/route.js
@@ -1,25 +1,29 @@
+import { saveMessageToDatabase } from "@/lib/message";
+
 export async function POST(req) { // It should be POST or GET
-   try {
-     const data = await req.json();
- 
-     // Send the data to the Glitch server
-     const response = await fetch("https://planet-fine-xenon.glitch.me/webhook", {
-       method: "POST",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify(data),
-     });
- 
-     const result = await response.json();
- 
-     return new Response(JSON.stringify(result), { status: response.status });
-   } catch (error) {
-     console.error("Error sending message:", error);
-     return new Response(
-       JSON.stringify({ error: "Failed to send message to Glitch server" }),
-       { status: 500 }
-     );
-   }
- }
- 
+  try {
+    const data = await req.json();
+    
+    // Send the data to the Glitch server
+    const response = await fetch("https://planet-fine-xenon.glitch.me/webhook", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    // saving message to database
+    const { to, text } = data;
+    const timestamp = Math.floor(Date.now() / 1000).toString()
+    saveMessageToDatabase(to, text, timestamp, 'integra')
+
+    return new Response({ status: response.status });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    return new Response(
+      JSON.stringify({ error: "Failed to send message to Glitch server" }),
+      { status: 500 }
+    );
+  }
+}
