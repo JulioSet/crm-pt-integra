04d48459f962ac6bc992ad3436d6d4b5d08cfde2
JulioSet <105445347+JulioSet@users.noreply.github.com>
2025-02-24 01:44:03 +0700

    update CRUD agent

--- src/lib/employee.ts
+++ src/lib/employee.ts
@@ -1,11 +1,10 @@
-// create new employee
-export async function createEmployee(name: string, password: string, role: string) {
+export async function createEmployee(id: string, name: string, password: string, role: string) {
    const response = await fetch('/api/auth/register', {
       method: 'POST',
       headers: {
          'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name, password, role }), // Send data as JSON
+      body: JSON.stringify({ id, name, password, role }), // Send data as JSON
    });
 
    if (!response.ok) {
@@ -14,7 +13,6 @@ export async function createEmployee(name: string, password: string, role: strin
    }
 }
 
-// get employee's data
 export async function getEmployee(name: string, password: string) {
    const response = await fetch('/api/auth/login', {
       method: 'POST',
@@ -35,4 +33,52 @@ export async function getEmployee(name: string, password: string) {
    }
    
    return { success: true, data: data };
+}
+
+export async function getEmployeeByRole(role: string) {
+   const response = await fetch('/api/auth/fetch', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ role }), // Send data as JSON
+   });
+   
+   if (!response.ok) {
+      console.error('Failed to fetch employee');
+      return;
+   }
+   
+   const { data } = await response.json();
+   return data;
+}
+
+export async function updateEmployee(id: string, name: string, target: number) {
+   const response = await fetch('/api/auth/update', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id, name, target }), // Send data as JSON
+   });
+   
+   if (!response.ok) {
+      console.error('Failed to update employee');
+      return;
+   }
+}
+
+export async function deleteEmployee(id: string) {
+   const response = await fetch('/api/auth/delete', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }), // Send data as JSON
+   });
+   
+   if (!response.ok) {
+      console.error('Failed to update employee');
+      return;
+   }
 }
