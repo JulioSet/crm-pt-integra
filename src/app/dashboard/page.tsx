2ba2b5422420736db0af36176544227b148f73d5
JulioSet <105445347+JulioSet@users.noreply.github.com>
2025-02-27 01:00:10 +0700

    delete code from session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,6 @@
-import { getGlobalSession } from "@/utils/global-session";
-
 export default function Dashboard() {
    // fetching session
    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-   const session = getGlobalSession();
 
    return (
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6 bg-white">
