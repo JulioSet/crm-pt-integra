155304fa8578217dfcf3bb454e42e44e69a90709
JulioSet <105445347+JulioSet@users.noreply.github.com>
2025-04-10 11:52:06 +0700

    fix chat name not synchronized with contact when contact deleted

--- src/app/api/contact/delete/route.ts
+++ src/app/api/contact/delete/route.ts
@@ -4,6 +4,20 @@ import { prisma } from '@/lib/prisma';
 export async function POST(req: NextRequest) {
    const { id } = await req.json(); // Read the body data
 
+   const data = await prisma.contact.findFirst({
+      where: { id: id },
+      select: {
+         telepon: true
+      }
+   })
+
+   await prisma.message_header.update({
+      where: { telepon: data?.telepon },
+      data: {
+         nama: null
+      }
+   })
+
    await prisma.contact.delete({
       where: { id: id }
    });
