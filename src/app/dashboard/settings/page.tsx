7c4bcbfe8e3edbf726ee3e989147c1b66a93f88b
JulioSet <105445347+JulioSet@users.noreply.github.com>
2025-05-16 04:07:16 +0700

    ubah nama settings

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -69,7 +69,7 @@ export default function SettingsPage() {
          ) : (
             <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
                <div className="flex items-center justify-between">
-                  <h2 className="text-3xl font-bold tracking-tight">Settings</h2>
+                  <h2 className="text-3xl font-bold tracking-tight">Pengaturan</h2>
                </div>
 
                <Card className="rounded-lg">
